b025189f82876289443ccec8866285f09ce2ac25
Gary Qin <garyzhqin@gmail.com>
2024-06-01 01:57:24 -0400

    setup firebase

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB37to1BKw213Rau-8PVschGmERthH5M9c",
+  authDomain: "collab-todo-list.firebaseapp.com",
+  projectId: "collab-todo-list",
+  storageBucket: "collab-todo-list.appspot.com",
+  messagingSenderId: "573947151762",
+  appId: "1:573947151762:web:43fe525890f50b0f79ad4b",
+  measurementId: "G-NGC953BW7M"
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+console.log("hello");
+
