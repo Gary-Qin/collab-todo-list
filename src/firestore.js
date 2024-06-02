c7708d690b31275b029608ae5bbd29e72eb85e8c
Gary Qin <garyzhqin@gmail.com>
2024-06-02 19:52:23 -0400

    implement deleting items

--- src/firestore.js
+++ src/firestore.js
@@ -1,32 +1,87 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { addDoc, collection, getFirestore, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
+
+let thingsRef;
+let database;
 
 const itemList = document.querySelector("#itemList");
-const addItem = document.querySelector("#addItem");
+const addItemButton = document.querySelector("#addItemBtn");
+const inputForm = document.querySelector("#inputForm");
 
-export async function setupFirestore(app, auth) {
-    const db = getFirestore(app);
-    let thingsRef;
+function toggleAddItem() {
+    inputForm.hasChildNodes() ? inputForm.replaceChildren() : createForm();
+}
 
+function createForm() {
+    const nameLabel = document.createElement("label");
+    const nameInput = document.createElement("input");
+    const priorityLabel = document.createElement("label");
+    const priorityInput = document.createElement("input");
+    const confirmButton = document.createElement("button");
+    
+    nameLabel.textContent = "List item: ";
+    nameInput.type = "text";
+    nameInput.id = "name";
+    nameInput.setAttribute("required", "");
+
+    priorityLabel.textContent = "Priority:  ";
+    priorityInput.type = "number";
+    priorityInput.id = "priority";
+    priorityInput.setAttribute("required", "");
+
+    confirmButton.textContent = "Confirm";
+    confirmButton.type = "submit";
+    confirmButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        writeToDB(nameInput.value, priorityInput.value);
+        nameInput.value = ``;
+        priorityInput.value = ``;
+    })
+
+    inputForm.appendChild(nameLabel)
+    inputForm.appendChild(nameInput);
+    inputForm.appendChild(priorityLabel)
+    inputForm.appendChild(priorityInput);
+    inputForm.appendChild(confirmButton);
+}
+
+async function writeToDB(name, priority) {
+    const docRef = await addDoc(collection(database, "things"), {
+        name: name,
+        priority: priority,
+        timestamp: serverTimestamp()
+    });
+    console.log("Document written with ID: ", docRef.id);
+}
+
+function displayDB() {
+    const q = query(collection(database, "things"), orderBy("timestamp", "asc"))
+    onSnapshot(q, querySnapshot => {
+        itemList.replaceChildren();
+        querySnapshot.forEach((item) => {
+
+            const listItem = document.createElement("li");
+            const deleteButton = document.createElement("button");
+            listItem.textContent = `${item.data().name}, Priority: ${item.data().priority}`;
+            deleteButton.textContent = "Delete item";
+            deleteButton.addEventListener("click", async () => await deleteDoc(doc(database, "things", item.id)))
+
+            itemList.appendChild(listItem);
+            itemList.appendChild(deleteButton);
+        })
+    })
+}
+
+export function setupFirestore(db, auth) {
     onAuthStateChanged(auth, user => {
         if(user) {
             try {
-                thingsRef = collection(db, "things");
-
-                addItem.addEventListener("click", async () => {
-                    const docRef = await addDoc(thingsRef, {
-                        name: "walk dog",
-                        priority: 3,
-                        timestamp: serverTimestamp()
-                    });
-                    console.log("Document written with ID: ", docRef.id);
-                })
-
+                database = db;
+                addItemBtn.addEventListener("click", () => toggleAddItem());
+                displayDB();
             } catch (e) {
                 console.error("Error adding document: ", e);
             }
         }
     })
-
-    
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 import { setupAuth } from "./auth.js";
 import { setupFirestore } from "./firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,7 +19,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 setupAuth(auth);
-setupFirestore(app, auth);
+setupFirestore(db, auth);
 
