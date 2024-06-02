16f8f485e528087e679e1d0a699c772a0aa5957f
Gary Qin <garyzhqin@gmail.com>
2024-06-02 07:53:04 -0400

    firebase initialization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-console.log("hello");
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
 
