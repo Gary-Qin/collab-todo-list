6af5bf7d8b4a2c5eba30527f9b173f1f96216ba9
Gary Qin <garyzhqin@gmail.com>
2024-06-06 23:43:59 -0400

    added list functionality + started storing users in list documents

--- src/firestore.js
+++ src/firestore.js
@@ -1,11 +1,14 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { addDoc, collection, deleteDoc, doc, getDoc, onSnapshot, orderBy, query, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
+import { addDoc, arrayUnion, collection, deleteDoc, doc, getDoc, onSnapshot, orderBy, query, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
 import "./style.css";
 
 const listsDiv = document.querySelector("#lists");
+const addListForm = document.querySelector("#listForm");
 let unsubscribe;
 let currentUser;
 let db;
+
+/* ===== MAINLY FIREBASE FUNCTIONS ===== */
 
 async function findUsersLists() {
     const usersSnap = await getDoc(doc(db, "users", currentUser.uid));
@@ -14,11 +17,11 @@ async function findUsersLists() {
         const listArray = usersSnap.data().accessibleLists
         return listArray;
     }
-    return initializeUser();
+    return initializeUserList();
 }
 
-async function initializeUser() {
-    const listsRef = await addDoc(collection(db, "lists"), {
+async function initializeUserList() {
+    const listRef = await addDoc(collection(db, "lists"), {
         title: `${currentUser.displayName}'s List`,
         description: serverTimestamp(),
         roles: {
@@ -26,7 +29,7 @@ async function initializeUser() {
         }           
     });
 
-    const itemsRef = await addDoc(collection(db, "lists", listsRef.id, "items"), {
+    const itemRef = await addDoc(collection(db, "lists", listRef.id, "items"), {
         user: "Bot",
         item: "Add to the to-do list!",
         priority: 1,
@@ -34,28 +37,75 @@ async function initializeUser() {
         timestamp: serverTimestamp()
     })
 
-    const usersRef = await setDoc(doc(db, "users", currentUser.uid), {
+    const userRef = await setDoc(doc(db, "users", currentUser.uid), {
         email: currentUser.email,
-        accessibleLists: [listsRef.id]
+        accessibleLists: [listRef.id]
     });
 
-    const listArray = [listsRef.id]
+    const listArray = [listRef.id]
     return listArray;
 }
 
-async function writeToDoc(listId, item, priority) {
-    const docRef = await addDoc(collection(db, "lists", listId, "items"), {
+async function writeToItems(listId, item, priority) {
+    const itemRef = await addDoc(collection(db, "lists", listId, "items"), {
         user: currentUser.displayName,
         item: item,
         priority: priority,
         accomplished: false,
         timestamp: serverTimestamp()
     });
-    console.log("Document written with ID: ", docRef.id);
+    console.log("Document written with ID: ", itemRef.id);
 }
 
+async function writeListIdToCurrentUser(listId) {
+    const userRef = await updateDoc(doc(db, "users", currentUser.uid), {
+        accessibleLists: arrayUnion(listId)
+    });
+}
+
+async function writeUIDtoListRoles(listId, roleType) {
+    const userRef = await updateDoc(doc(db, "lists", listId), {
+        [`roles.${currentUser.uid}`]: roleType
+    });
+}
+
+function giveUserAccessToList(listId) {
+    writeListIdToCurrentUser(listId);
+    writeUIDtoListRoles(listId, "editor")
+}
+
+async function fetchAndDisplayContent() {
+    // findUsersLists returns a Promise for an array of list ids that should be accessible to the user
+    const usersLists = await findUsersLists(currentUser, db);
+    listsDiv.replaceChildren();
+
+    displayListId(usersLists[0]);
+    activateAddListButton();
+    usersLists.forEach(async (listId) => {
+        await createListElements(listId);
+        displayListInRealTime(listId);
+    });
+}
+
+export function setupFirestore(database, auth) {
+    db = database;
+    onAuthStateChanged(auth, user => {
+        if(user) {
+            currentUser = user;
+            fetchAndDisplayContent();
+        }
+        else {
+            unsubscribe && unsubscribe();
+        }
+    });
+}
+
+
+
+/* ===== DISPLAYING TO-DO LIST UI FUNCTIONS ===== */
+
 function displayListInRealTime(listId) {
-    const itemList = document.querySelector(`.${listId}`);
+    const itemList = document.querySelector(`ul.${listId}`);
     const q = query(collection(db, "lists", listId, "items"), orderBy("timestamp", "asc"));
 
     unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -85,37 +135,6 @@ function displayListInRealTime(listId) {
     });
 }
 
-async function fetchAndDisplayContent() {
-    // findUsersLists returns a Promise for an array of list ids that should be accessible to the user
-    const usersLists = await findUsersLists(currentUser, db);
-    listsDiv.replaceChildren();
-
-    displayListId(usersLists[0]);
-    usersLists.forEach(async (listId) => {
-        await createListElements(listId);
-        displayListInRealTime(listId);
-    });
-}
-
-export function setupFirestore(database, auth) {
-    db = database;
-    onAuthStateChanged(auth, user => {
-        if(user) {
-            currentUser = user;
-            fetchAndDisplayContent();
-        }
-        else {
-            unsubscribe && unsubscribe();
-        }
-    });
-}
-
-
-
-
-
-/* ===== PURE UI FUNCTIONS ===== */
-
 async function createListElements(listId) {
     const listContainer = document.createElement("div");
     const listTitle = document.createElement("h3");
@@ -124,11 +143,14 @@ async function createListElements(listId) {
     const addItemForm = document.createElement("form");
     const docSnap = await getDoc(doc(db, "lists", listId));
 
+    listContainer.className = listId;
+    actualList.className = listId;
+    addItemForm.className = "itemForm";
+
     listTitle.textContent = docSnap.data().roles[currentUser.uid] === "owner" ? "Your List" : docSnap.data().title;
     addItemButton.textContent = "Add item";
     addItemButton.addEventListener("click", () => toggleAddItem(listId, addItemForm));
-    actualList.className = listId;
-    addItemForm.className = "listForm";
+    
     
     listContainer.appendChild(listTitle);
     listContainer.appendChild(actualList);
@@ -138,10 +160,10 @@ async function createListElements(listId) {
 }
 
 function toggleAddItem(listId, form) {
-    form.hasChildNodes() ? form.replaceChildren() : createForm(listId, form);
+    form.hasChildNodes() ? form.replaceChildren() : createItemForm(listId, form);
 }
 
-function createForm(listId, form) {
+function createItemForm(listId, form) {
     const itemLabel = document.createElement("label");
     const itemInput = document.createElement("input");
     const priorityLabel = document.createElement("label");
@@ -162,7 +184,7 @@ function createForm(listId, form) {
     confirmButton.type = "submit";
     confirmButton.addEventListener("click", (e) => {
         e.preventDefault();
-        writeToDoc(listId, itemInput.value, priorityInput.value);
+        writeToItems(listId, itemInput.value, priorityInput.value);
         itemInput.value = ``;
         priorityInput.value = ``;
     })
@@ -174,10 +196,15 @@ function createForm(listId, form) {
     form.appendChild(confirmButton);
 }
 
+
+
+/* ===== OTHER UI FUNCTIONS ===== */
+
 function displayListId(listId) {
     const displayIdElement = document.querySelector("#listId");
     const idText = document.createElement("span");
     const toggleIdButton = document.createElement("button");
+    displayIdElement.replaceChildren();
 
     toggleIdButton.addEventListener("click", () => {
         idText.textContent = idText.textContent === "Your list ID: " ? `Your list ID: ${listId}` : "Your list ID: ";
@@ -188,4 +215,41 @@ function displayListId(listId) {
     
     displayIdElement.appendChild(idText);
     displayIdElement.appendChild(toggleIdButton);
+}
+
+function activateAddListButton() {
+    const addListButton = document.querySelector("#addListButton");
+    addListButton.addEventListener("click", () => toggleAddList())
+}
+
+function toggleAddList() {
+    // TO-DO: do not show list form if 3 lists already exist
+    addListForm.hasChildNodes() ? addListForm.replaceChildren() : createListForm();
+}
+
+function createListForm() {
+    const listLabel = document.createElement("label");
+    const listInput = document.createElement("input");
+    const confirmButton = document.createElement("button");
+    
+    listLabel.textContent = "List ID: ";
+    listInput.type = "text";
+    listInput.id = "listID";
+    listInput.setAttribute("required", "");
+
+    confirmButton.textContent = "Confirm";
+    confirmButton.type = "submit";
+    confirmButton.addEventListener("click", async (e) => {
+        e.preventDefault();
+        // TO-DO: verify if list ID is valid/already exists
+        giveUserAccessToList(listInput.value)
+        await createListElements(listInput.value);
+        displayListInRealTime(listInput.value);
+        // TO-DO: verify if list ID is valid/already exists
+        toggleAddList();
+    })
+
+    addListForm.appendChild(listLabel)
+    addListForm.appendChild(listInput);
+    addListForm.appendChild(confirmButton);
 }
